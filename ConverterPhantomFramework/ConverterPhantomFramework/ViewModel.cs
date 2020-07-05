f5f36c57a2615da789c5ea171ad6458fc8388db7
Хасп Элд <EldHasp@Gmail.com>
2020-07-05 22:58:38 +0300

    Add ClearMethod

--- ConverterPhantomFramework/ConverterPhantomFramework/ViewModel.cs
+++ ConverterPhantomFramework/ConverterPhantomFramework/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 
 namespace ConverterPhantomFramework
 {
@@ -15,12 +16,21 @@ namespace ConverterPhantomFramework
         public Range Range { get => _range; set => Set(ref _range, value); }
 
         private RelayCommand _clearCommand;
-        public RelayCommand ClearCommand => _clearCommand ?? (_clearCommand = new RelayCommand(p => Squares.Clear()));
+        public RelayCommand ClearCommand => _clearCommand ?? (_clearCommand = new RelayCommand(ClearMethod));
+
+        private void ClearMethod(object parameter)
+        {
+            for (int i = 0; i < Squares.Count; i++)
+                Squares[i] = null;
+
+            Squares.Clear();
+            
+        }
 
         private RelayCommand _resizeCommand;
-        public RelayCommand ResizeCommand => _resizeCommand ?? (_resizeCommand = new RelayCommand(ResizeMetod));
+        public RelayCommand ResizeCommand => _resizeCommand ?? (_resizeCommand = new RelayCommand(ResizeMethod));
 
-        private void ResizeMetod(object parameter)
+        private void ResizeMethod(object parameter)
         {
             Range = new Range(0, 5);
         }
